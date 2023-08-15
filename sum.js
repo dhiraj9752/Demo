8dde5258d8ec25ddca0a5b3464d36afd4e9d6ca3
Dhiraj <dhiraj9752@gmail.com>
2023-08-15 13:34:55 +0530

    Added to new demo

--- sum.js
+++ sum.js
@@ -8,3 +8,5 @@ console.log(sub);
 console.log(mul);
 console.log("abcd");
 console.log('hello! I guess you know me ')
+console.log('hello! I guess you know me ')
+console.log('hello! I guess you know me ')
